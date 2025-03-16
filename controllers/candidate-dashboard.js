bf044eddd42ed7449b752ef574be25672e4262bb
Abhijith C S <writetoabhijith@gmail.com>
2025-03-16 21:25:51 +0530

    refactor

---
+++ controllers/candidate-dashboard.js
@@ -0,0 +1,31 @@
+angular.module('CandidateDashboardApp', ['ngCookies'])
+
+.config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}])
+
+
+.controller('candidateDashboardController', function($scope, $http, $interval, $cookies) {
+
+    //Check if logged in
+    if($cookies.get("crispriteUserToken")){
+      $scope.isLoggedIn = true;
+    }
+    else{
+      $scope.isLoggedIn = false;
+      window.location = "profile.html";
+    }
+
+    //Logout function
+    $scope.logoutNow = function(){
+      if($cookies.get("crispriteUserToken")){
+        $cookies.remove("crispriteUserToken");
+        window.location = "index.html";
+      }
+    }
+
+    function getUserToken() {
+      return "Bearer " + $cookies.get("crispriteUserToken");  
+    }
+
+});
