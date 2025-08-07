aeb534ebc043f76f8097f78eebd3ee0aebce2eb3
Abhijith C S <writetoabhijith@gmail.com>
2025-08-07 17:53:45 +0530

    Fix

--- course/course.js
+++ course/course.js
@@ -14,43 +14,26 @@ $(document).ready(function() {
         if(!player)
             player = new playerjs.Player('bunny-stream-embed');
 
-        // Initialize variables to store total duration and last progress point
         let totalDuration = 0;
         let lastProgress = 0;
 
-        // Event handler when the player is ready
         player.on('ready', () => {
             console.log('Ready');
         });
 
-        // Event handler when the video is played
         player.on('play', () => {
             console.log('Video is playing');
         });
 
-        // Get the total duration of the video and start playing
         player.getDuration((duration) => {
             totalDuration = duration;
         });
 
-        // Event handler for time updates when the player is playing
         player.on('timeupdate', (timingData) => {
 
-        // Get current seconds
-        const currentTime = timingData.seconds;
-
-        // Calculate progress percentage and round to the nearest 25%
-        const progressPercentage = (currentTime / timingData.duration) * 100;
-        const progressRounded = Math.floor(progressPercentage / 25) * 25;
-
-        // Log the progress percentage
-        console.log('Progress Percentage: ' + Math.floor(progressPercentage) + "%");
-
-        // Check if progress reached a new 25% milestone and update the progress bar
-        if (progressRounded > lastProgress) {
-            console.log(`Video progress: ${progressRounded}%`);
-            lastProgress = progressRounded;
-        }
+            const currentTime = timingData.seconds;
+            const progressPercentage = Math.floor((currentTime / timingData.duration) * 100);
+            console.log('Progress Percentage: ' +progressPercentage+ "%");
                 
         });
     }
