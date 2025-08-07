e6355ccbcadebe9acf060dd890f7c3335d0016a5
Abhijith C S <writetoabhijith@gmail.com>
2025-08-07 15:37:04 +0530

    Test

--- course/course.js
+++ course/course.js
@@ -14,11 +14,69 @@ $(document).ready(function() {
     selectedPartId = selectedPartId && selectedPartId >= 0 ? selectedPartId : 0;
 
 
+    function trackProgress() {
+
+        console.log('tracking...')
+
+        // Create a PlayerJS instance with the 'bunny-stream-embed' element
+        const player = new playerjs.Player('bunny-stream-embed');
+
+        // Initialize variables to store total duration and last progress point
+        let totalDuration = 0;
+        let lastProgress = 0;
+
+        // Event handler when the player is ready
+        player.on('ready', () => {
+            console.log('Ready');
+        });
+
+        // Event handler when the video is played
+        player.on('play', () => {
+            console.log('Video is playing');
+        });
+
+        // Get the total duration of the video and start playing
+        player.getDuration((duration) => {
+            totalDuration = duration;
+        });
+
+        // Event handler for time updates when the player is playing
+        player.on('timeupdate', (timingData) => {
+
+        // Get current seconds
+        const currentTime = timingData.seconds;
+
+        // Calculate progress percentage and round to the nearest 25%
+        const progressPercentage = (currentTime / timingData.duration) * 100;
+        const progressRounded = Math.floor(progressPercentage / 25) * 25;
+
+        // Log the progress percentage
+        console.log('Progress Percentage: ' + Math.floor(progressPercentage) + "%");
+
+        // Update the progress text on the page
+        progressText.textContent = `${Math.floor(progressPercentage)}%`;
+
+        // Check if progress reached a new 25% milestone and update the progress bar
+        if (progressRounded > lastProgress) {
+            console.log(`Video progress: ${progressRounded}%`);
+            lastProgress = progressRounded;
+        }
+                
+        });
+    }
+
+
+
 
     function renderContentVideo(contentSource) {
         document.getElementById("videoRenderSpace").innerHTML = '' +
             '<iframe id="bunny-stream-embed" src="https://iframe.mediadelivery.net/embed/475938/'+contentSource+'" width="720" height="400" frameborder="0" allow="autoplay"></iframe>';
+    
+        setTimeout(function () {
+          trackProgress();
+        }, 3000);
     }
+
 
 
     function renderSideBarAndVideo(moduleData, chapterData, selectedPartData) {
